8524d74516b0e112fec44dd4e3647c9631444d9c
g13 <g1bs0n@mail.ru>
2020-11-06 16:06:59 +0200

    добавлено решение от coursera

--- week_2/to_json.py
+++ week_2/to_json.py
@@ -5,3 +5,19 @@ def to_json(x):
     def inn_function(*args, **kwargs):
         return json.dumps(x(*args, **kwargs))
     return inn_function
+
+
+'''
+import functools
+import json
+
+
+def to_json(func):
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return json.dumps(result)
+
+    return wrapped
+'''
